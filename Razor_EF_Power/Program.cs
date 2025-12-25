9fe260bb0da1e87a4dc5779bf582f999eabc74e7
Sanya <kartuzovaleksandr@yandex.ru>
2025-12-25 19:39:21 +0300

    Добавьте файлы проекта.

---
+++ Razor_EF_Power/Program.cs
@@ -0,0 +1,10 @@
+﻿using Razor_EF_Power.Models;
+
+using var context = new Razor_EFContext();
+
+var clients = context.Products.ToList();
+
+foreach (var p in clients)
+{
+    Console.WriteLine($"{p.Name} {p.Price}" );
+}
